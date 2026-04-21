65eb89c031bfdee9f2715dce821b30646b5bb90d
Shantosh Durai <shantoshdurai06@gmail.com>
2026-04-22 01:25:26 +0530

    Add firebase_options.dart - required for build
    Firebase configuration contains public client-side keys (App ID, Project ID, etc.)
    that are meant to be bundled with the app. Using custom PigeonFirebaseOptions
    class name requires explicit inclusion in the repo.
    
    Updated .gitignore to exclude only truly sensitive secrets (.env files).

---
+++ lib/firebase_options.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class PigeonFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError('Web is not configured. Use FlutterFire CLI.');
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        throw UnsupportedError('iOS is not configured. Use FlutterFire CLI.');
+      case TargetPlatform.macOS:
+        throw UnsupportedError('macOS is not configured. Use FlutterFire CLI.');
+      default:
+        throw UnsupportedError(
+          'PigeonFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyD1-EdouHHKdVb9PsgoX4vwM_HHvW3Won0',
+    appId: '1:67687510848:android:a559c193f49651800d40a5',
+    messagingSenderId: '67687510848',
+    projectId: 'studio-4155999944-16272',
+    storageBucket: 'studio-4155999944-16272.firebasestorage.app',
+  );
+}
